449c926d75d50b52bd7efbd46bd247af5cf32501
DarthNeo <morrissoid.james@hotmail.com>
2015-05-09 14:33:49 -0400

    Missed one..

--- al_setup.py
+++ al_setup.py
@@ -7,14 +7,14 @@ import re
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-setup(name=u'al_Archilib',
-      version=u'0.1',
-      description=u'Tools for Processing Archimate Models',
-      url=u'http://github.com/darth-neo/ArchiConcepts',
-      author=u'Darth Neo',
+setup(name='al_Archilib',
+      version='0.1',
+      description='Tools for Processing Archimate Models',
+      url='http://github.com/darth-neo/ArchiConcepts',
+      author='Darth Neo',
       author_email=u'morrisspid.james@gmail.com',
-      license=u'MIT',
-      packages=[u'al_ArchiLib'],
+      license='MIT',
+      packages=['al_ArchiLib'],
       zip_safe=False,
 
       classifiers=[
@@ -22,27 +22,27 @@ setup(name=u'al_Archilib',
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        u'Development Status :: 3 - Alpha',
+        'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
-        u'Intended Audience :: Developers',
-        u'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        u'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        u'Programming Language :: Python :: 2',
-        u'Programming Language :: Python :: 2.6',
-        u'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ],
 
     # What does your project relate to?
-    keywords=u'Archimate XML Models Processing',
+    keywords='Archimate XML Models Processing',
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed.
-    install_requires = [u'nltk', u'networkx', u'py2neo', u'lxml', u'openpyxl', u'pygraphviz', u'python-pptx', u'python-docx'],)
+    install_requires = ['nltk', 'networkx', 'py2neo', 'lxml', 'openpyxl', 'pygraphviz', 'python-pptx', 'python-docx'],)
 
 
