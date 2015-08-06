9279fb619b2e14ea22e92e22d48e12f2756e22e2
morrisspid.james@hotmail.com <morrisspid.james@hotmail.com>
2015-08-06 12:03:55 -0400

    Fixed mailto

--- al_CleanArchiHTML.py
+++ al_CleanArchiHTML.py
@@ -1,7 +1,7 @@
 #! env python
 # __author__ = 'morrj140'
 
-from BeautifulSoup import *
+from bs4 import *
 
 from al_lib.Logger import *
 logger = setupLogging(__name__)
@@ -9,11 +9,13 @@ logger.setLevel(INFO)
 
 def outputSoup(soup, filename=u"index.html"):
     with open(filename, u'w') as f:
-        f.write(soup.prettify())
+        utf8_S = soup.prettify().encode("utf-8", errors="ignore")
+        ascii_S = utf8_S.decode("ascii", errors="ignore")
+        f.write(ascii_S)
 
 if __name__ == u"__main__":
 
-    dirHTML = u"/Users/morrj140/Documents/SolutionEngineering/Archimate Models/DVC2.18/"
+    dirHTML = u"/Users/morrj140/Documents/SolutionEngineering/Archimate Models/DVC3.1/"
     fileHTML = u"model.html"
     indexHTML = dirHTML + u"index.html"
 
@@ -25,8 +27,21 @@ if __name__ == u"__main__":
 
     soup = BeautifulSoup(html)
 
-    sp = soup.find(u"span", {u"class": u"i18n-relations"}).parent
+    #
+    # Insert MailTo
+    # - <a href="mailto:james.m.morris@disney.com?Subject=DVC" target="_top">Inaccuracies?</a>
+    # "/html/body/div[1]/nav/div[1]/span"
+    sp = soup.body.div.nav.div.span
 
+    st = soup.new_tag(u"a")
+    st[u"href"] = u"mailto:james.m.morris@disney.com?Subject=DVC\" target=\"_top\""
+    st.string = u"Inaccuracies?"
+
+    sp.append(st)
+
+    #
+    # Remove relations due to size
+    sp = soup.find("span", {u"class": u"i18n-relations"}).parent
     sp.extract()
 
     outputSoup(soup, indexHTML)
